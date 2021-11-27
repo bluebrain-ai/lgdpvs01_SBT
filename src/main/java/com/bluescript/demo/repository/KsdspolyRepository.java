ab51b53e335c6828f3916ebbc6c41303285b030e
bluebrain-ai <bluebrainenterprise@gmail.com>
2021-11-27 15:15:53 +0530

    first commit

---
+++ src/main/java/com/bluescript/demo/repository/KsdspolyRepository.java
@@ -0,0 +1,21 @@
+package com.bluescript.demo.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bluescript.demo.entity.KsdsPolyEntity;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+@Repository
+public interface KsdspolyRepository extends CrudRepository<KsdsPolyEntity, String> {
+
+  // void deleteByWfRequestIdAndIdWfPolicyNumAndIdWfCustomerNum(String wfRequestId, String wfPolicyNum,
+  //     String wfCustomerNum);
+@Transactional
+@Modifying(clearAutomatically = true)
+@Query(value = "DELETE FROM KSDSPOLY WHERE REQUESTID = ? AND CUSTOMERNUM =? AND POLICYNUM = ?",nativeQuery = true)
+void deleteByKsdsPolyKey(String wfRequestId, String wfCustomerNum,String wfPolicyNum);
+}
